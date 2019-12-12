d5af83a8771d8babad19c93566bc7467f84287d0
Robert Knecht <mrbknecht@gmail.com>
2019-12-12 01:35:23 -0600

    getting reasonable data structures back

--- process-scores.php
+++ process-scores.php
@@ -12,10 +12,24 @@ function getFrameScore($score1, $score2 = false) {
     return $score1 + $score2;
 }
 function checkStrike($index, $score_arr) {
-    //
+    if (!isset($score_arr[$index + 1])) {
+        return false;
+    }
+    $frame_score = 10 + $score_arr[$index + 1]['first'];
+    if (isset($score_arr[$index + 1]['second'])) {
+        $frame_score += $score_arr[$index + 1]['second'];
+    } else if (isset($score_arr[$index + 2])) {
+        $frame_score += $score_arr[$index + 2]['first'];
+    } else {
+        return false;
+    }
+    return $frame_score;
 }
 function checkSpare($index, $score_arr) {
-    //
+    if (!isset($score_arr[$index + 1])) {
+        return false;
+    }
+    return 10 + $score_arr[$index + 1]['first'];
 }
 
 $score = $_POST['scores'][0];
@@ -44,12 +58,28 @@ foreach ($frame_arr as $key => $frame) {
         'frameScore' => getFrameScore($first, $second)
     ];
 }
+$return_arr = [];
 $score_total = 0;
 foreach ($score_arr as $key => $score) {
     if ($score['frameScore'] == 'strike') {
-
+        $score['frameScore'] = checkStrike($key, $score_arr);
     }
+    if ($score['frameScore'] == 'spare') {
+        $score['frameScore'] = checkSpare($key, $score_arr);
+    }
+    if (!$score['frameScore']) {
+        $running_total = false;
+    } else {
+        $score_total = $score_total + $score['frameScore'];
+        $running_total = $score_total;
+    }
+    
+    $return_arr["$key"] = [
+        'first' => $score['first'],
+        'second' => $score['second'],
+        'running_total' => $running_total
+    ];
 }
-echo json_encode($score_arr);
+echo json_encode($return_arr);
 
 ?>
